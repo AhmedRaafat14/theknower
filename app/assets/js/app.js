4dd10e8d8d2ebffe16910a5f8bdfc04feea46bb8
Ahmed Raafat <a.raafat@global.leaseweb.com>
2020-04-21 15:22:09 +0200

    Enable the bootstrap, fontawesome and bootstrap-social buttons libraries to work with the webpack encore

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -8,7 +8,8 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+// Need jQuery? then uncomment to import it.
+import $ from 'jquery';
+import 'bootstrap';
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
